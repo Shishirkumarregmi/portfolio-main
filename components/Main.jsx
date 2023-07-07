2f8fa227e8a0b7da81647eddb081506415f5cd25
Shishirkumarregmi <68994454+Shishirkumarregmi@users.noreply.github.com>
2023-07-07 12:59:26 -0400

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
             LET&#39;S AI SOMETHING NEW TOGETHER
           </p>
           <h1 className="py-4 text-gray-700">
-            Hi, I&#39;m <span className="text-[#5651e5]"> Sankalpa</span>
+            Hi, I&#39;m <span className="text-[#5651e5]">Shishir</span>
           </h1>
           <h1 className="py-2 text-gray-700">A Tech Enthusiast</h1>
           <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
